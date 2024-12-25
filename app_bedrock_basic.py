80fb76c475efb1487e621a5c28c0e2d4c4783a0b
Saugat Bhattarai <the.saggitorres@gmail.com>
2024-12-25 14:43:24 -0500

    Basic code to integrate aws bedrock with claude3 and langchain

--- app_bedrock_basic.py
+++ app_bedrock_basic.py
@@ -1,20 +1,21 @@
 #Basic Code for Bedrock App with langchain
+#Runs with the default profile
 
 import boto3
 from langchain_aws import ChatBedrock
-import os
 
-os.environ["aws-profile"] = "aws-toolkit-local"
 client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
 model_id = "anthropic.claude-3-haiku-20240307-v1:0"
-# model_kwargs =  { 
-#     "max_tokens": 1024,
-#     "temperature": 0.9,
-# }
+model_kwargs =  { 
+    "max_tokens": 1024,
+    "temperature": 0.9,
+}
+
 claude_v2_client = ChatBedrock(
     client=client,
     model_id=model_id,
-    # model_kwargs=model_kwargs,
+    model_kwargs=model_kwargs,
 )
-response = claude_v2_client.invoke("who is buddha?")
-print(response)
+
+response = claude_v2_client.invoke("what is large learning model?")
+print(response.content)
